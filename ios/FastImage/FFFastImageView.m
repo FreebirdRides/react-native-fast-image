898ec0359af2c156b295e23e51db6a3afaa7c187
theonetheycallneo <anthony.wentzel@gmail.com>
2019-07-11 13:29:35 -0700

    fix: ios

--- ios/FastImage/FFFastImageView.m
+++ ios/FastImage/FFFastImageView.m
@@ -1,18 +1,11 @@
 #import "FFFastImageView.h"
 
-@interface FFFastImageView()
-
-@property (nonatomic, assign) BOOL hasSentOnLoadStart;
-@property (nonatomic, assign) BOOL hasCompleted;
-@property (nonatomic, assign) BOOL hasErrored;
-// Whether the latest change of props requires the image to be reloaded
-@property (nonatomic, assign) BOOL needsReload;
-
-@property (nonatomic, strong) NSDictionary* onLoadEvent;
-
-@end
-
-@implementation FFFastImageView
+@implementation FFFastImageView {
+    BOOL hasSentOnLoadStart;
+    BOOL hasCompleted;
+    BOOL hasErrored;
+    NSDictionary* onLoadEvent;
+}
 
 - (id) init {
     self = [super init];
@@ -21,122 +14,82 @@
     return self;
 }
 
-- (void)setResizeMode:(RCTResizeMode)resizeMode {
+- (void)setResizeMode:(RCTResizeMode)resizeMode
+{
     if (_resizeMode != resizeMode) {
         _resizeMode = resizeMode;
         self.contentMode = (UIViewContentMode)resizeMode;
     }
 }
 
-- (void)setOnFastImageLoadEnd:(RCTDirectEventBlock)onFastImageLoadEnd {
+- (void)setOnFastImageLoadEnd:(RCTBubblingEventBlock)onFastImageLoadEnd {
     _onFastImageLoadEnd = onFastImageLoadEnd;
-    if (self.hasCompleted) {
+    if (hasCompleted) {
         _onFastImageLoadEnd(@{});
     }
 }
 
-- (void)setOnFastImageLoad:(RCTDirectEventBlock)onFastImageLoad {
+- (void)setOnFastImageLoad:(RCTBubblingEventBlock)onFastImageLoad {
     _onFastImageLoad = onFastImageLoad;
-    if (self.hasCompleted) {
-        _onFastImageLoad(self.onLoadEvent);
+    if (hasCompleted) {
+        _onFastImageLoad(onLoadEvent);
     }
 }
 
 - (void)setOnFastImageError:(RCTDirectEventBlock)onFastImageError {
     _onFastImageError = onFastImageError;
-    if (self.hasErrored) {
+    if (hasErrored) {
         _onFastImageError(@{});
     }
 }
 
-- (void)setOnFastImageLoadStart:(RCTDirectEventBlock)onFastImageLoadStart {
-    if (_source && !self.hasSentOnLoadStart) {
+- (void)setOnFastImageLoadStart:(RCTBubblingEventBlock)onFastImageLoadStart {
+    if (_source && !hasSentOnLoadStart) {
         _onFastImageLoadStart = onFastImageLoadStart;
         onFastImageLoadStart(@{});
-        self.hasSentOnLoadStart = YES;
+        hasSentOnLoadStart = YES;
     } else {
         _onFastImageLoadStart = onFastImageLoadStart;
-        self.hasSentOnLoadStart = NO;
-    }
-}
-
-- (void)setImageColor:(UIColor *)imageColor {
-    if (imageColor != nil) {
-        _imageColor = imageColor;
-        super.image = [self makeImage:super.image withTint:self.imageColor];
-    }
-}
-
-- (UIImage*)makeImage:(UIImage *)image withTint:(UIColor *)color {
-    UIImage *newImage = [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
-    UIGraphicsBeginImageContextWithOptions(image.size, NO, newImage.scale);
-    [color set];
-    [newImage drawInRect:CGRectMake(0, 0, image.size.width, newImage.size.height)];
-    newImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return newImage;
-}
-
-- (void)setImage:(UIImage *)image {
-    if (self.imageColor != nil) {
-        super.image = [self makeImage:image withTint:self.imageColor];
-    } else {
-        super.image = image;
+        hasSentOnLoadStart = NO;
     }
 }
 
 - (void)sendOnLoad:(UIImage *)image {
-    self.onLoadEvent = @{
-                         @"width":[NSNumber numberWithDouble:image.size.width],
-                         @"height":[NSNumber numberWithDouble:image.size.height]
-                         };
-    if (self.onFastImageLoad) {
-        self.onFastImageLoad(self.onLoadEvent);
+    onLoadEvent = @{
+                    @"width":[NSNumber numberWithDouble:image.size.width],
+                    @"height":[NSNumber numberWithDouble:image.size.height]
+                    };
+    if (_onFastImageLoad) {
+        _onFastImageLoad(onLoadEvent);
     }
 }
 
 - (void)setSource:(FFFastImageSource *)source {
     if (_source != source) {
         _source = source;
-        _needsReload = YES;
-    }
-}
-
-- (void)didSetProps:(NSArray<NSString *> *)changedProps
-{
-    if (_needsReload) {
-        [self reloadImage];
-    }
-}
-
-- (void)reloadImage
-{
-    _needsReload = NO;
-
-    if (_source) {
-
+        
         // Load base64 images.
         NSString* url = [_source.url absoluteString];
         if (url && [url hasPrefix:@"data:image"]) {
-            if (self.onFastImageLoadStart) {
-                self.onFastImageLoadStart(@{});
-                self.hasSentOnLoadStart = YES;
+            if (_onFastImageLoadStart) {
+                _onFastImageLoadStart(@{});
+                hasSentOnLoadStart = YES;
             } {
-                self.hasSentOnLoadStart = NO;
+                hasSentOnLoadStart = NO;
             }
             UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:_source.url]];
             [self setImage:image];
-            if (self.onFastImageProgress) {
-                self.onFastImageProgress(@{
-                                           @"loaded": @(1),
-                                           @"total": @(1)
-                                           });
+            if (_onFastImageProgress) {
+                _onFastImageProgress(@{
+                                       @"loaded": @(1),
+                                       @"total": @(1)
+                                       });
             }
-            self.hasCompleted = YES;
+            hasCompleted = YES;
             [self sendOnLoad:image];
             
-            if (self.onFastImageLoadEnd) {
-                self.onFastImageLoadEnd(@{});
+            if (_onFastImageLoadEnd) {
+                _onFastImageLoadEnd(@{});
             }
             return;
         }
@@ -147,7 +100,8 @@
         }];
         
         // Set priority.
-        SDWebImageOptions options = SDWebImageRetryFailed;
+        SDWebImageOptions options = 0;
+        options |= SDWebImageRetryFailed;
         switch (_source.priority) {
             case FFFPriorityLow:
                 options |= SDWebImageLowPriority;
@@ -165,57 +119,58 @@
                 options |= SDWebImageRefreshCached;
                 break;
             case FFFCacheControlCacheOnly:
-                options |= SDWebImageFromCacheOnly;
+                options |= SDWebImageCacheMemoryOnly;
                 break;
             case FFFCacheControlImmutable:
                 break;
         }
         
-        if (self.onFastImageLoadStart) {
-            self.onFastImageLoadStart(@{});
-            self.hasSentOnLoadStart = YES;
+        if (_onFastImageLoadStart) {
+            _onFastImageLoadStart(@{});
+            hasSentOnLoadStart = YES;
         } {
-            self.hasSentOnLoadStart = NO;
+            hasSentOnLoadStart = NO;
         }
-        self.hasCompleted = NO;
-        self.hasErrored = NO;
+        hasCompleted = NO;
+        hasErrored = NO;
         
-        [self downloadImage:_source options:options];
-    }
-}
-
-- (void)downloadImage:(FFFastImageSource *) source options:(SDWebImageOptions) options {
-    __weak typeof(self) weakSelf = self; // Always use a weak reference to self in blocks
-    [self sd_setImageWithURL:_source.url
-            placeholderImage:nil
-                     options:options
-                    progress:^(NSInteger receivedSize, NSInteger expectedSize, NSURL * _Nullable targetURL) {
-                        if (weakSelf.onFastImageProgress) {
-                            weakSelf.onFastImageProgress(@{
-                                                           @"loaded": @(receivedSize),
-                                                           @"total": @(expectedSize)
-                                                           });
-                        }
-                    } completed:^(UIImage * _Nullable image,
-                                  NSError * _Nullable error,
-                                  SDImageCacheType cacheType,
-                                  NSURL * _Nullable imageURL) {
-                        if (error) {
-                            weakSelf.hasErrored = YES;
-                                if (weakSelf.onFastImageError) {
-                                    weakSelf.onFastImageError(@{});
-                                }
-                                if (weakSelf.onFastImageLoadEnd) {
-                                    weakSelf.onFastImageLoadEnd(@{});
-                                }
-                        } else {
-                            weakSelf.hasCompleted = YES;
-                            [weakSelf sendOnLoad:image];
-                            if (weakSelf.onFastImageLoadEnd) {
-                                weakSelf.onFastImageLoadEnd(@{});
+        // Load the new source.
+        // This will work for:
+        //   - https://
+        //   - file:///var/containers/Bundle/Application/50953EA3-CDA8-4367-A595-DE863A012336/ReactNativeFastImageExample.app/assets/src/images/fields.jpg
+        //   - file:///var/containers/Bundle/Application/545685CB-777E-4B07-A956-2D25043BC6EE/ReactNativeFastImageExample.app/assets/src/images/plankton.gif
+        //   - file:///Users/dylan/Library/Developer/CoreSimulator/Devices/61DC182B-3E72-4A18-8908-8A947A63A67F/data/Containers/Data/Application/AFC2A0D2-A1E5-48C1-8447-C42DA9E5299D/Documents/images/E1F1D5FC-88DB-492F-AD33-B35A045D626A.jpg"
+        [self sd_setImageWithURL:_source.url
+                placeholderImage:nil
+                         options:options
+                        progress:^(NSInteger receivedSize, NSInteger expectedSize, NSURL * _Nullable targetURL) {
+                            if (_onFastImageProgress) {
+                                _onFastImageProgress(@{
+                                                       @"loaded": @(receivedSize),
+                                                       @"total": @(expectedSize)
+                                                       });
                             }
-                        }
-                    }];
+                        } completed:^(UIImage * _Nullable image,
+                                      NSError * _Nullable error,
+                                      SDImageCacheType cacheType,
+                                      NSURL * _Nullable imageURL) {
+                            if (error) {
+                                hasErrored = YES;
+                                if (_onFastImageError) {
+                                    _onFastImageError(@{});
+                                }
+                                if (_onFastImageLoadEnd) {
+                                    _onFastImageLoadEnd(@{});
+                                }
+                            } else {
+                                hasCompleted = YES;
+                                [self sendOnLoad:image];
+                                if (_onFastImageLoadEnd) {
+                                    _onFastImageLoadEnd(@{});
+                                }
+                            }
+                        }];
+    }
 }
 
 @end
